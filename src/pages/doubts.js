4de71280d86d74a6ae4ac9692f7a1b944fe2c900
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-04-11 12:43:36 -0300

    create doubts page

--- src/pages/doubts.js
+++ src/pages/doubts.js
@@ -47,11 +47,12 @@ export default function Doubts() {
       </session>
 
       <footer className={styles.footer}>
-        <a href="https://adv.rodneyrinaldi.com">adv.rodneyrinaldi.com</a>
-        <a href="https://rodneyrinaldi.com"><img src="/rr-logo.svg"
+        <a href="https://dev.rodneyrinaldi.com">dev.rodneyrinaldi.com</a>
+        <a href="https://rodneyrinaldi.com"><img src="/rr-logo-dev.svg"
           alt="rodneyrinaldi" /></a>
         <a href="https://dpo.rodneyrinaldi.com">dpo.rodneyrinaldi.com</a>
       </footer>
+
 
     </div>
   )
