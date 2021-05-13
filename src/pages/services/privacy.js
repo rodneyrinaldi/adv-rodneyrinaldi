b91b10aa3a63531580b4ec56506998d403e42574
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-05-13 15:30:18 -0300

    services  pages

--- src/pages/services/privacy.js
+++ src/pages/services/privacy.js
@@ -4,7 +4,7 @@ import Contact from '../../components/contact'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
 
-import styles from './services.module.css'
+import styles from '../../styles/services.module.css'
 
 export default function Family() {
   return (
@@ -12,11 +12,19 @@ export default function Family() {
       <Contact showback='yes' />
       <main className={styles.main}>
         <Header />
-        <div className={styles.title}>
+        <div className={styles.code}>
           <h1>Ações de família</h1>
         </div>
         <div className={styles.grid}>
+          <div>
+            xxx
+          </div>
+          <div>
+            xxx
+          </div>
 
+          <p>yyy</p>
+          <p>yyy</p>
         </div>
       </main>
       <Footer />
