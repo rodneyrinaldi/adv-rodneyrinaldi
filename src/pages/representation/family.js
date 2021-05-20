a3876f15bef36293057badc9a8e354f7df5e3fb3
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-05-20 20:41:50 -0300

    create family representation page

--- src/pages/representation/family.js
+++ src/pages/representation/family.js
@@ -11,7 +11,7 @@ export default function Family() {
     <Layout showback='yes'>
       <Title title="Ações de família" />
       <div className={styles.row}>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea dignissimos magnam quo debitis rerum illo et ad, doloremque eum voluptatem sunt. Laborum laudantium a quidem quaerat consequuntur tempora libero.</p>
+        <h1>Segundo a Constituição Federal a família é base da sociedade e tem especial proteção do Estado, art 226.</h1>
         <div className={styles.image}>
           <Image
             src="/article-family.png"
@@ -20,13 +20,14 @@ export default function Family() {
             height={300}
           />
         </div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea dignissimos magnam quo debitis rerum illo et ad, doloremque eum voluptatem sunt. Laborum laudantium a quidem quaerat consequuntur tempora libero.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea dignissimos magnam quo debitis rerum illo et ad, doloremque eum voluptatem sunt. Laborum laudantium a quidem quaerat consequuntur tempora libero.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea dignissimos magnam quo debitis rerum illo et ad, doloremque eum voluptatem sunt. Laborum laudantium a quidem quaerat consequuntur tempora libero.</p>
+        <p>E para a sociologia a família é representada por uma agregação de indivíduos unidos por laços afetivos ou de parentesco, com o objetivo de desenvolvimento mútuo e que os adultos são responsáveis pelo cuidado com as crianças.</p>
+        <p>Por este motivo todas as decisões referentes ao assunto devem ser permeadas pela civilidade e urbanismo, lenvando-se em consideração o princípio do melhor interesse para a criança conforme entendimento do Superior Tribunal de Justiça.</p>
+        <p></p>
+
         <a href='/settings'>
           Gerar o contrato e as procurações
         </a>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea dignissimos magnam quo debitis rerum illo et ad, doloremque eum voluptatem sunt. Laborum laudantium a quidem quaerat consequuntur tempora libero.</p>
+        <p></p>
       </div>
     </Layout>
   </>)
