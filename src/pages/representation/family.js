d44762b6be8e85ac2b2553ca2080adae16b149e6
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-05-20 15:33:54 -0300

    create family representation page

--- src/pages/representation/family.js
+++ src/pages/representation/family.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 import Layout from '../../components/layout'
 import Title from '../../components/title'
@@ -11,9 +12,20 @@ export default function Family() {
       <Title title="Ações de família" />
       <div className={styles.row}>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea dignissimos magnam quo debitis rerum illo et ad, doloremque eum voluptatem sunt. Laborum laudantium a quidem quaerat consequuntur tempora libero.</p>
+        <div className={styles.image}>
+          <Image
+            src="/article-family.png"
+            alt="Família"
+            width={800}
+            height={300}
+          />
+        </div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea dignissimos magnam quo debitis rerum illo et ad, doloremque eum voluptatem sunt. Laborum laudantium a quidem quaerat consequuntur tempora libero.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea dignissimos magnam quo debitis rerum illo et ad, doloremque eum voluptatem sunt. Laborum laudantium a quidem quaerat consequuntur tempora libero.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea dignissimos magnam quo debitis rerum illo et ad, doloremque eum voluptatem sunt. Laborum laudantium a quidem quaerat consequuntur tempora libero.</p>
+        <a href='/settings'>
+          Gerar o contrato e as procurações
+        </a>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ea dignissimos magnam quo debitis rerum illo et ad, doloremque eum voluptatem sunt. Laborum laudantium a quidem quaerat consequuntur tempora libero.</p>
       </div>
     </Layout>
