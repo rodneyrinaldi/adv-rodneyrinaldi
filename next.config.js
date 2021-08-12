4ec6254995acd97d1f885b058fa18e8e80ba1096
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-08-12 02:04:25 -0300

    update project v2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
     return config
   },
+
+  assetPrefix: isProd ? 'https://cdn.rodneyrinaldi.com' : '',
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <>
       <div className={styles.container}>
-        <a href="https://adv.rodneyrinaldi.com">adv.rodneyrinaldi.com</a>
+        <a href="https://dev.rodneyrinaldi.com">dev.rodneyrinaldi.com</a>
         <a href="https://rodneyrinaldi.com"><img src="/rr-ico.svg"
           alt="rodneyrinaldi" /></a>
         <a href="https://dpo.rodneyrinaldi.com">dpo.rodneyrinaldi.com</a>
