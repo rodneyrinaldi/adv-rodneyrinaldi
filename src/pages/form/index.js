688d8d20bd2644cc740e7ffbf0a4b2dc4c7a82e1
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-09-03 19:08:59 -0300

    adding google forms integration

--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -1,18 +1,22 @@
 import React from 'react'
 
+import Layout from '../../components/layout'
+
 import styles from './index.module.css'
 
 function Form() {
   return (
     <>
-      <div className={styles.row}>
-        <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLSeCj8ThYcpzcuHhyRQ_EYxRdSsP2aTavDdsbbqnOG9pE4D-2A/viewform?embedded=true"
-          width="100%" height="100%"
-          frameborder="0" marginheight="0" marginwidth="0">
-          Carregando…
-        </iframe>
-      </div>
+      <Layout showback='yes'>
+        <div className={styles.row}>
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSeCj8ThYcpzcuHhyRQ_EYxRdSsP2aTavDdsbbqnOG9pE4D-2A/viewform?embedded=true"
+            width="100%" height="100%"
+            frameborder="0" marginheight="0" marginwidth="0">
+            Carregando…
+          </iframe>
+        </div>
+      </Layout>
     </>
   )
 }
