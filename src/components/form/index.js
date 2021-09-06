7e0963457b9e535bb8ca18f7166127a7ab4c8f12
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-09-06 18:07:05 -0300

    adding google forms integration

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,20 +1,27 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 import data from '../../data/urls.json'
 
 function Form(props) {
-  let url = ""
-  data.map(function (item) {
-    if (item.title == props.title) {
-      url = 'https://docs.google.com/forms/' + item.url + '/viewform?embedded=true'
-      return
-    }
-  })
+  const [url, setUrl] = useState('/d/e/1FAIpQLSf08kUi0ebwEVh1iLoX2gkJnXqhRml83nzlUlKqqWVeniTVNA/');
+
+  useEffect(() => {
+    data.map(function (item) {
+      if (item.title == props.title) {
+        if (item.url !== '-') {
+          setUrl(item.url)
+        }
+        return
+      }
+    })
+  }, []);
+
 
   return (
     <>
       <iframe
-        src={url}
+        src={'https://docs.google.com/forms' + url + 'viewform?embedded=true'}
         width='100%' height='100%'
         frameBorder='0' marginHeight='0' marginWidth='0'>
         Carregando…
--- src/pages/documentForm/index.js
+++ src/pages/documentForm/index.js
@@ -1,23 +1,26 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
+import Form from '../../components/form'
 import Layout from '../../components/layout'
 import Title from '../../components/title'
 
-import data from '../../data/skills.json'
-
 import styles from './index.module.css'
 
-function Skills() {
+function Document() {
+  const router = useRouter()
+  const { query: { title }, } = router
+
   return (
     <>
       <Layout showback='yes'>
-        <Title title="HABILIDADES" />
+        <Title title={title} />
         <div className={styles.row}>
-
+          <Form title={title} />
         </div>
       </Layout>
     </>
   )
 }
 
-export default Skills
+export default Document
