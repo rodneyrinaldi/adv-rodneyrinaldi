c86ae17942a6de0e415ed8186b9969c08d7252a4
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2022-03-23 20:47:20 -0300

    adding meta tags control

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,38 +1,57 @@
-import React from 'react';
-import Head from 'next/head'
-import Link from 'next/link'
-
-import styles from './index.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import Meta from "../meta";
+import styles from "./index.module.css";
 
 function Header(props) {
-  return (<>
+  return (
+    <>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>adv.rodneyrinaldi</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Meta
+        title="Rodney Rinaldi Advogado"
+        descrition="Advocacia moderna, eficiente e ética"
+        image="https://adv.rodneyrinaldi.com/card.svg"
+        url="https://adv.rodneyrinaldi.com/"
+      />
+
       <div className={styles.container}>
         <div>
           <img src="/rr-adv.svg" alt="rodneyrinaldi" className={styles.logo1} />
         </div>
-        <div >
-          <img src="/rodneyrinaldi.svg" alt="rodneyrinaldi" className={styles.logo2} />
+        <div>
+          <img
+            src="/rodneyrinaldi.svg"
+            alt="rodneyrinaldi"
+            className={styles.logo2}
+          />
         </div>
         <div>
           <div className={styles.advm}>ADV</div>
           <div className={styles.adv}>ADVOGADO</div>
         </div>
       </div>
-      {props.showcard === 'yes'
-        ? <Link href='/card'>
-          <a href='/card' className={styles.card}>
-            <img src="/rr-card.svg" alt="cartão visitas" className={styles.logo3} />
+      {props.showcard === "yes" ? (
+        <Link href="/card">
+          <a href="/card" className={styles.card}>
+            <img
+              src="/rr-card.svg"
+              alt="cartão visitas"
+              className={styles.logo3}
+            />
           </a>
         </Link>
-        : <></>
-      }
-  </>)
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 
-export default Header
+export default Header;
